b5e65b068ec212859235407dfee97bdedb4903ec
Konstantin Vyatkin <tino@vtkn.io>
2020-06-02 00:28:59 -0400

    :lipstick:

--- api/src/index.js
+++ api/src/index.js
@@ -1,7 +1,11 @@
 import { once } from "events";
+
 import { app } from "./app.js";
 import connectMongoDB from "./mongodb.js";
 
+// connect MongoDB before starting server
+await connectMongoDB(app);
+// start listening
 const server = app.listen(process.env.PORT || 3030);
-await Promise.all([once(server, "listening"), connectMongoDB(app)]);
+await once(server, "listening");
 app.context.log.info(`Server is listening on port ${server.address().port}`);
