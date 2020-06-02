ae037b9fee2f0535045992d01945407930df05da
Konstantin Vyatkin <tino@vtkn.io>
2020-06-02 00:27:16 -0400

    implemented login

---
+++ api/src/routes/login/index.js
@@ -0,0 +1,48 @@
+import Router from "koa-router";
+import bcrypt from "bcrypt";
+import normalizeEmail from "validator/lib/normalizeEmail.js";
+import jwt from "jsonwebtoken";
+
+export const loginRoutes = new Router({ prefix: "/login" });
+
+loginRoutes.post(
+  "/",
+  /**
+   * @param {KoaContext} ctx
+   * @param {Function} next
+   */
+  async (ctx, next) => {
+    const { email, password } = ctx.request.body;
+    ctx.assert(
+      typeof email === "string" && email.length > 3,
+      422,
+      `User email is required`
+    );
+    ctx.assert(
+      typeof password === "string" && password.length > 1,
+      422,
+      `Password is required`
+    );
+    // getting user from db
+    const user = await ctx.db
+      .collection("Users")
+      .findOne(
+        { email: normalizeEmail(email), status: "active" },
+        { projection: { password_hash: 1, email: 1, roles: 1 } }
+      );
+    ctx.assert(user, 401, `User ${user} is unknown or inactive`);
+    const match = await bcrypt.compare(password, user.password_hash);
+    ctx.assert(match, 401, `Invalid password or username`);
+
+    const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "5h" });
+    ctx.status = 200;
+    ctx.body = {
+      token,
+    };
+
+    // save it to the cookie too
+    ctx.cookies.set("jwt", token);
+
+    return next();
+  }
+);
---
+++ api/test/login.js
@@ -0,0 +1,68 @@
+import { once } from "events";
+import bcrypt from "bcrypt";
+import request from "supertest";
+import chai from "chai";
+
+import connectMongoDb from "../src/mongodb.js";
+import { app } from "../src/app.js";
+
+const { expect } = chai;
+
+describe("login unprotected routes", function () {
+  /** @type {import('http').Server} */
+  let server;
+  let knownUserEmail = "test@user.com";
+  let knownUserPassword = "test12244";
+
+  before(async function () {
+    // connecting to db
+    await connectMongoDb(app);
+    // launch server
+    server = app.listen(0);
+    await once(server, "listening");
+    // creating known user
+    await app.context.db.collection("Users").findOneAndUpdate(
+      {
+        email: knownUserEmail,
+      },
+      {
+        $set: {
+          email: knownUserEmail,
+          password_hash: bcrypt.hashSync(knownUserPassword, 10),
+          status: "active",
+        },
+      },
+      { upsert: true }
+    );
+  });
+
+  after(async function () {
+    server.close();
+  });
+
+  it("should return token for known user", async function () {
+    await request(server)
+      .post("/login")
+      .set("Content-Type", "application/json")
+      .send({
+        email: knownUserEmail.toUpperCase(),
+        password: knownUserPassword,
+      })
+      .expect("Content-Type", /json/)
+      .expect((res) => {
+        expect(res.body).to.be.string;
+      })
+      .expect(200);
+  });
+
+  it("should return 401 for unknown user", async function () {
+    await request(server)
+      .post("/login")
+      .set("Content-Type", "application/json")
+      .send({
+        email: knownUserEmail,
+        password: knownUserPassword.slice(1),
+      })
+      .expect(401);
+  });
+});
